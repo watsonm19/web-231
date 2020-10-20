02dec32f47963dcf3271ede5f68d8b57361714b6
Mark Watson <watsonm19@me.com>
2020-10-20 15:15:13 -0700

    assignment 1.4

---
+++ week-1/watson-primitive-types.js
@@ -0,0 +1,13 @@
+var firstName = "Mark";
+var lastName = "Watson";
+var age = 27;
+var street = "123 Willow Ave";
+var city = "Portland";
+var zipCode = "77777"
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
