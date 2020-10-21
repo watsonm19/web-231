9640df5e905f0ab593d9594fa1eb35a312dbecae
Mark Watson <watsonm19@me.com>
2020-10-21 14:52:58 -0700

    discussion 1.1 solution

---
+++ week-1/watson-values-variables-solution.js
@@ -0,0 +1,19 @@
+/* 
+;========================================================
+; Title: Discussion 1.1 - Values and Variables
+; Author: Mark Watson
+; Date: October 21, 2020
+; Description: Basic JavaScript code meant to have two errors within it for revision.
+;========================================================
+*/
+
+var changeThisNumber = 99;
+
+changeThisNumber++; 
+//the first letter 'c' needed to be changed from uppercase to lowercase to match the original variable above
+
+
+for (i = 0; i < changeThisNumber; i++) {
+    console.log(i);
+}
+//proper use of a for loop requires a ';' in between the statements and not '|'
