c6a37999e6f0c4058b2970d215c2fef919c9169e
Mark Watson <watsonm19@me.com>
2020-10-21 15:48:47 -0700

    assignment 1.5

---
+++ week-1/watson-1.5-types-values-variables.js
@@ -0,0 +1,50 @@
+//=============
+// Employee 1
+//=============
+let firstName = 'Willow';
+let lastName = 'Louise';
+let address = '123 Bean Rd';
+let payRate = (16.06).toFixed(1);
+let hireDate = new Date(2018, 5, 16);
+
+console.log("====================");
+console.log("Employee 1");
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Address: " + address);
+console.log("Pay Rate: $" + payRate);
+console.log("Hire Date: " + hireDate);
+
+//=============
+// Employee 2
+//=============
+firstName = 'Nathan';
+lastName = 'Raymond';
+address = '456 Mayo Ave';
+payRate = (31.10).toFixed(1);
+hireDate = new Date(1998, 9, 31);
+
+console.log("====================");
+console.log("Employee 2");
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Address: " + address);
+console.log("Pay Rate: " + "$" + payRate);
+console.log("Hire Date: " + hireDate);
+
+//=============
+// Employee 3
+//=============
+firstName = 'Vanessa';
+lastName = 'Sofia';
+address = '789 Net St';
+payRate = (18.77).toFixed(1);
+hireDate = new Date(2000, 6, 18);
+
+console.log("====================");
+console.log("Employee 3");
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Address: " + address);
+console.log("Pay Rate: " + "$" + payRate);
+console.log("Hire Date: " + hireDate);
