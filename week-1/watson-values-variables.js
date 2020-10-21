cd201d3d41f813f0d80d3d280714c5b3bb3ef27c
Mark Watson <watsonm19@me.com>
2020-10-21 14:47:22 -0700

    discussion 1.1

---
+++ week-1/watson-values-variables.js
@@ -0,0 +1,17 @@
+/* 
+;========================================================
+; Title: Discussion 1.1 - Values and Variables
+; Author: Mark Watson
+; Date: October 21, 2020
+; Description: Basic JavaScript code meant to have two errors within it for revision.
+;========================================================
+*/
+
+var changeThisNumber = 99;
+
+ChangeThisNumber++;
+
+
+for (i = 0 | i < changeThisNumber | i++) {
+    console.log(i);
+}
