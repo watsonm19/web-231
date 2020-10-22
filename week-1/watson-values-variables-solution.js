b3c5086a5fafc0467aee9931a97002605a589893
Mark Watson <watsonm19@me.com>
2020-10-21 18:04:21 -0700

    discussion 1.1 solution edited and changed

--- week-1/watson-values-variables-solution.js
+++ week-1/watson-values-variables-solution.js
@@ -3,17 +3,24 @@
 ; Title: Discussion 1.1 - Values and Variables
 ; Author: Mark Watson
 ; Date: October 21, 2020
-; Description: Basic JavaScript code with solutions
+; Description: Basic JavaScript code with solutions to discussion 1.1
 ;========================================================
 */
 
 var changeThisNumber = 99;
 
-changeThisNumber++; 
-//the first letter 'c' needed to be changed from uppercase to lowercase to match the original variable above
+var changeThisNumber = 99;
+var changeThisNumberMore = 51;
+var makeThisNumberSmall = 200;
 
+changeThisNumber++;
+// This variable needed a lowercase 'c' like in the original call, rather than an uppercase.
 
-for (i = 0; i < changeThisNumber; i++) {
-    console.log(i);
-}
-//proper use of a for loop requires a ';' in between the statements and not '|'
+changeThisNumberMore *= 2;
+// The operators in this line were in reverse order '=*' which creates an error. To correct this, the arithmetic operator '*' needs to come before the assignment operator '='
+
+makeThisNumberSmall = makeThisNumberSmall / 10;
+
+console.log(changeThisNumber);
+console.log(changeThisNumberMore);
+console.log(makeThisNumberSmall);
