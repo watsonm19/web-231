751f40bab6ed28a45aec14e1764d869a3cc93737
Mark Watson <watsonm19@me.com>
2020-10-29 14:55:32 -0700

    added requirements for Exercise 2.2

---
+++ week-2/watson-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Mark Watson
+; Date:   29 October 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// exports header.js file for use
+const header = require('../watson-header.js');
+
+// logs header to console for display
+console.log(header.display('Mark', 'Watson', 'Exercise 2.2'));
